09b5f6d8e1988936b8ffc411d86a7b39249aa92c
Rainbow Asteroids <rainbow@portal2d.xyz>
2023-06-14 16:33:19 -0400

    Moved some stuff around. Added revert to default feature for dynamically generated inspector properties.

--- addons/customizable_packed_scene/customizable_packed_scene.gd
+++ addons/customizable_packed_scene/customizable_packed_scene.gd
@@ -1,4 +1,5 @@
 @tool
+@icon("res://addons/customizable_packed_scene/icon.svg")
 extends Resource
 class_name CustomizablePackedScene
 
@@ -12,10 +13,10 @@ var overrides: Dictionary = {}
 
 func _get(property):
 	if scene != null:
-		if not property in overrides:
-			return scene.instantiate().get_script().get_property_default_value(property)
-		else:
+		if property in overrides:
 			return overrides[property]
+		elif scene.instantiate().get_script().get_property_default_value(property) != null:
+			return scene.instantiate().get(property)
 
 func _set(property, value):
 	if scene != null:
@@ -30,7 +31,16 @@ func _get_property_list():
 		)
 	else:
 		return []
-	
+
+func _property_can_revert(property):
+	return property in overrides
+
+func _property_get_revert(property):
+	if property in overrides:
+		return scene.instantiate().get(property)
+	else:
+		return null
+
 func instantiate() -> Node:
 	var node = scene.instantiate()
 	
@@ -38,3 +48,5 @@ func instantiate() -> Node:
 		node.set(property, overrides[property])
 	
 	return node
+
+
